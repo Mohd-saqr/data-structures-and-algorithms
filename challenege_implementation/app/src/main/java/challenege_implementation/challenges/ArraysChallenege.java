9bd508354b59d5baedc3d8d8051cd36a8531b6ba
msqer998 <saqerabu9@gmail.com>
2022-05-15 03:03:49 +0300

    edit readme file

--- challenege_implementation/app/src/main/java/challenege_implementation/challenges/ArraysChallenege.java
+++ challenege_implementation/app/src/main/java/challenege_implementation/challenges/ArraysChallenege.java
@@ -1,2 +1,45 @@
-package challenege_implementation.challenges;public class ArraysChallenege {
+package challenege_implementation.challenges;
+
+public class ArraysChallenege {
+
+    public int[] reversArray(int[] arr) {
+        int[] newArr = new int[arr.length];
+        int count = 0;
+        for (int i = arr.length-1; i >= 0; i--) {
+            newArr[count] = arr[i];
+            count++;
+        }
+        return newArr;
+    }
+
+    public int[] insertShiftArray(int[]arr, int value){
+        int mid=(arr.length/2);
+        int []newArr = new int[arr.length+1];
+        for (int i=0;i< arr.length;i++){
+            if (i==mid){
+                newArr[i]=value;
+                continue;
+            }
+            else newArr[i]=arr[i];
+
+        }
+        return newArr;
+    }
+
+    public int []findSumRowArray(int [][] arr){
+        int [] newArr= new int[arr.length];
+        int sum = 0;
+        for (int i=0;i<arr.length;i++){
+            for (int j=0;i<arr[j].length;j++){
+                sum+=arr[i][j];
+            }
+            newArr[i]=sum;
+            sum=0;
+        }
+        return newArr;
+    }
+    public int Fibonacci(int n){
+        if (n<=1)return n;
+       return Fibonacci(n-1)+Fibonacci(n-2);
+    }
 }
--- challenege_implementation/app/src/main/java/challenege_implementation/datastructures/LinkedList.java
+++ challenege_implementation/app/src/main/java/challenege_implementation/datastructures/LinkedList.java
@@ -1,2 +1,115 @@
-package challenege_implementation.datastructures;public class LinkedList {
+package challenege_implementation.datastructures;
+
+public class LinkedList<T> {
+
+   private Node<T>head;
+   private int size=0;
+
+    public class Node<T>{
+       private  T value;
+       private  Node<T> next;
+
+        public Node(T value) {
+            this.value = value;
+        }
+
+        public T getValue() {
+            return value;
+        }
+
+        public void setValue(T value) {
+            this.value = value;
+        }
+
+        public Node<T> getNext() {
+            return next;
+        }
+
+        public void setNext(Node<T> next) {
+            this.next = next;
+        }
+
+    }
+
+    public LinkedList() {
+        this.head=null;
+    }
+
+    public void addFirst(T value){
+        Node<T> node = new Node<>(value);
+        if (this.head==null){
+            this.head=node;
+            incrementSize();
+        }
+        node.next=head.next;
+        head=node;
+        incrementSize();
+    }
+
+
+    public void addLast(T value){
+        Node<T> pointer =head;
+        while (pointer.next!=null){
+            pointer=pointer.next;
+        }
+        pointer.next= new Node<>(value);
+        incrementSize();
+    }
+
+    public boolean contain(T value){
+        Node<T> pointer =head;
+        while (pointer!=null){
+            if (pointer.value==value)return true;
+            pointer=pointer.next;
+        }
+        return false;
+    }
+
+    public void addAfter(T value,T after){
+        Node<T> pointer =head;
+        Node<T>node = new Node<>(value);
+        if (!contain(after))return ;
+        while (pointer!=null){
+            if (pointer.value==after){
+                Node<T>temp=pointer.next;
+                pointer.next=node;
+                node.next=temp;
+                return;
+            }
+            pointer=pointer.next;
+        }
+    }
+
+    public void remove(T value){
+        Node<T> pointer =head;
+        while (pointer!=null){
+            if (pointer.next.value==value){
+                pointer.next=pointer.next.next;
+                return;
+            }
+            pointer=pointer.next;
+        }
+    }
+    public void insertBefore(T value,T before){
+        Node<T> pointer =head;
+        Node<T> next=head.next;
+        Node<T> node = new Node<>(value);
+        if (value== head.value){
+            node.next=head;
+            head=node;
+        }
+        while (pointer!=null){
+            if (pointer.next.value==before){
+                pointer.next=node;
+                node.next=next;
+                return;
+            }
+            next=next.next;
+            pointer=pointer.next;
+        }
+    }
+
+    private void incrementSize() {
+        size++;
+    }
 }
